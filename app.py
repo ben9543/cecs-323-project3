ee893e0c439091cf884c68a688367a89cec4c880
Jordan Hilado <jordanalihilado@gmail.com>
2023-05-09 09:44:51 -0700

    finish troop lookup

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient, database
 
-HOST = "localhost"
-PORT = "27017"
-DATABASE_URL = f"mongodb://{HOST}:{PORT}"
+# HOST = "localhost"
+# PORT = "27017"
+# DATABASE_URL = f"mongodb://{HOST}:{PORT}"
+DATABASE_URL = "mongodb+srv://cecs327assignment:cecs327assignment@cluster0.bsjkxmo.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = "project3"
 collections = {
     "adults": "adults",
@@ -25,8 +26,44 @@ def ExecuteAggregation(db, collectionName, pipline):
         print(e)
         return None
 
-db = OpenConnection()
-print(db.list_collection_names())
+def main():
+    choice = int(input("Main Menu\n1. Troop Lookup\n2. Scout Lookup\n3. Sales Report\n>"))
+    if choice == 1:
+        troopNum = int(input("Enter Troop Number: "))
+        db = OpenConnection()
+        collectionName = collections["troops"]
+        pipline = [{"$match":{"_id": troopNum}}]
+        troop = ExecuteAggregation(
+            db=db, 
+            collectionName=collectionName, 
+            pipline=pipline
+        )
+        print("\nTroop Number: {}\n- Founding Date: {}\n- Community: {}\n- Number of Scouts: {}\n- Number of Volunteers: {}\nScouts:".format(
+            troop[0]["_id"],
+            troop[0]["founding_date"],
+            troop[0]["community"],
+            len(troop[0]["scouts"]),
+            len(troop[0]["volunteers"])
+        ))
+        for scout in troop[0]["scouts"]:
+            print("- Name: {}".format(scout["firstname"] + " " + scout["lastname"]))
+        print("Volunteers:")
+        for volunteer in troop[0]["volunteers"]:
+            print("- Name: {} (Position: {})".format(volunteer["firstname"] + " " + volunteer["lastname"], volunteer["position"]))
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    else:
+        print("Invalid Choice, try again")
+        while choice != 1 and choice != 2 and choice != 3:
+            choice = int(input("Main Menu\n1. Troop Lookup\n2. Scout Lookup\n3. Sales Report\n>"))
+
+if __name__ == "__main__":
+    main()
+
+# db = OpenConnection()
+# print(db.list_collection_names())
 
 # collectionName = collections["adults"]
 # pipline = [{"$match":{}}]
